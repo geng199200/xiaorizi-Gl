bef17b21f0bdd2c854767364765b252326ea6171
GL <1247759034@qq.com>
2016-11-04 17:02:43 +0800

    add wkwebview progress

--- xiaorizi-Gl/Controller/Main/Controller/MainDetailViewController.swift
+++ xiaorizi-Gl/Controller/Main/Controller/MainDetailViewController.swift
@@ -8,21 +8,50 @@
 
 import UIKit
 import WebKit
+import SnapKit
 
 class MainDetailViewController: UIViewController {
     public var itemModel: ItemModel?
     var wkWebView: WKWebView!
+    let progress: UIProgressView = {
+        let progress = UIProgressView()
+        progress.tintColor = UIColor.red
+        progress.progress = 0
+        progress.alpha = 0
+        return progress
+    } ()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
         configWebView()
+        self.wkWebView.addObserver(self, forKeyPath: "estimatedProgress", options: NSKeyValueObservingOptions.new, context: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        self.wkWebView.removeObserver(self, forKeyPath: "estimatedProgress")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: KVO
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progress.alpha = 1
+            self.progress.setProgress(Float(self.wkWebView.estimatedProgress), animated:true)
+            if self.wkWebView.estimatedProgress >= 1.0 {
+                UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveEaseInOut, animations: {
+                    self.progress.alpha = 0
+                    }, completion: { (finshed: Bool) in
+                        self.progress.progress = 0
+                })
+            }
+        }
     }
 
     //MARK: Private Method
@@ -40,13 +69,18 @@ class MainDetailViewController: UIViewController {
             make.bottom.equalTo(self.view.snp.bottom).offset(0)
             make.top.equalTo(self.view.snp.top).offset(0)
         }
+
+        self.wkWebView.addSubview(self.progress)
+        self.progress.snp.makeConstraints { (make) in
+            make.left.equalTo(self.wkWebView.snp.left)
+            make.top.equalTo(self.wkWebView.snp.top).offset(64)
+            make.right.equalTo(self.wkWebView.snp.right)
+            make.height.equalTo(5)
+        }
+
         let url = URL.init(string: (self.itemModel?.url)!)
         let request = URLRequest.init(url: url!)
         self.wkWebView.load(request)
 
     }
-    
-
-
-
 }
